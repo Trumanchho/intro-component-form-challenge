3b0ed56aa6423d79145fcdf982db31504c915126
Truman Ho <132316164+Trumanchho@users.noreply.github.com>
2023-05-08 21:47:24 -0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,75 @@
+
+const regex = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+
+function checkFields() {
+    const firstName = document.getElementById('first-name');
+    const lastName = document.getElementById('last-name');
+    const email = document.getElementById('email');
+    const password = document.getElementById('password');
+    const firstErr = document.getElementById('first-empty');
+    const lastErr = document.getElementById('last-empty');
+    const emailErr = document.getElementById('email-empty');
+    const passwordErr = document.getElementById('password-empty');
+
+    let validFirstName = false;
+    let validLastName = false;
+    let validEmail = false;
+    let validPassword = false;
+
+    if (firstName.value == null || firstName.value == "") {
+        firstName.style.backgroundImage = "url(./images/icon-error.svg)";
+        firstName.style.borderColor = "#ff7a7a";
+        firstErr.style.display = "block";
+        firstName.classList.add("transparent");
+    } else {
+        firstName.style.backgroundImage = "none";
+        firstName.style.borderColor = "lightgrey";
+        firstErr.style.display = "none";
+        validFirstName = true;
+    }
+    if (lastName.value == null || lastName.value == "") {
+        lastName.style.backgroundImage = "url(./images/icon-error.svg)";
+        lastName.style.borderColor = "#ff7a7a";
+        lastErr.style.display = "block";
+        lastName.classList.add("transparent");
+    } else {
+        lastName.style.backgroundImage = "none";
+        lastName.style.borderColor = "lightgrey";
+        lastErr.style.display = "none";
+        validLastName = true;
+    }
+    if (email.value == null || email.value == "") {
+        email.style.backgroundImage = "url(./images/icon-error.svg)";
+        email.style.borderColor = "#ff7a7a";
+        emailErr.innerHTML = "Email Address cannot be empty";
+        emailErr.style.display = "block";
+        email.classList.add("transparent");
+    } else if (!regex.test(email.value)) {
+        email.style.backgroundImage = "url(./images/icon-error.svg)";
+        email.style.borderColor = "#ff7a7a";
+        email.style.color = "#ff7a7a";
+        emailErr.innerHTML = "Looks like this is not an email";
+        emailErr.style.display = "block";
+    } else {
+        email.style.backgroundImage = "none";
+        email.style.borderColor = "lightgrey";
+        email.style.color = "#3e3c49";
+        emailErr.style.display = "none";
+        validEmail = true;
+    }
+    if (password.value == null || password.value == "") {
+        password.style.backgroundImage = "url(./images/icon-error.svg)";
+        password.style.borderColor = "#ff7a7a";
+        passwordErr.style.display = "block";
+        password.classList.add("transparent");
+    } else {
+        password.style.backgroundImage = "none";
+        password.style.borderColor = "lightgrey";
+        passwordErr.style.display = "none";
+        validPassword = true;
+    }
+
+    if (validEmail && validFirstName && validLastName && validPassword) {
+        alert("You have claimed you free trial.");
+    }
+}
---
+++ style-guide.md
@@ -0,0 +1,35 @@
+# Front-end Style Guide
+
+## Layout
+
+The designs were created to the following widths:
+
+- Mobile: 375px
+- Desktop: 1440px
+
+## Colors
+
+### Primary
+
+- Red: hsl(0, 100%, 74%) #ff7a7a
+- Green: hsl(154, 59%, 51%) #38cc8c
+
+### Accent
+
+- Blue: hsl(248, 32%, 49%) #6055a5
+#
+### Neutral
+
+- Dark Blue: hsl(249, 10%, 26%) #3e3c49
+- Grayish Blue: hsl(246, 25%, 77%) #b9b6d3
+
+## Typography
+
+### Body Copy
+
+- Font size: 16px
+
+### Font
+
+- Family: [Poppins](https://fonts.google.com/specimen/Poppins)
+- Weights: 400, 500, 600, 700
